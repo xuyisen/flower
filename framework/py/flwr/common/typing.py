9d8d6a28e687bb4fd7793fc3f60a6b9f29c085f3
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-21 22:39:26 -0400

    Muna4029__diff__id_118

--- framework/py/flwr/common/typing.py
+++ framework/py/flwr/common/typing.py
@@ -17,15 +17,14 @@
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Callable, Optional, Union
+from typing import Any, Callable, Optional, TypeAlias, Union
 
 import numpy as np
-import numpy.typing as npt
 
-NDArray = npt.NDArray[Any]
-NDArrayInt = npt.NDArray[np.int_]
-NDArrayFloat = npt.NDArray[np.float64]
-NDArrays = list[NDArray]
+NDArray: TypeAlias = np.ndarray[Any, np.dtype[Any]]
+NDArrayInt: TypeAlias = np.ndarray[Any, np.dtype[np.int_]]
+NDArrayFloat: TypeAlias = np.ndarray[Any, np.dtype[np.float64]]
+NDArrays: TypeAlias = list[NDArray]
 
 # The following union type contains Python types corresponding to ProtoBuf types that
 # ProtoBuf considers to be "Scalar Value Types", even though some of them arguably do
