34cfc2ae4724523b7fdbe7fa105d865a34258a5b
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-21 22:52:09 -0400

    Muna4029__diff__id_114

--- framework/py/flwr/common/secure_aggregation/ndarrays_arithmetic.py
+++ framework/py/flwr/common/secure_aggregation/ndarrays_arithmetic.py
@@ -18,7 +18,7 @@
 from typing import Any, Union
 
 import numpy as np
-from numpy.typing import DTypeLike, NDArray
+from numpy.typing import NDArray
 
 
 def factor_combine(factor: int, parameters: list[NDArray[Any]]) -> list[NDArray[Any]]:
@@ -39,7 +39,7 @@ def get_parameters_shape(parameters: list[NDArray[Any]]) -> list[tuple[int, ...]
 
 
 def get_zero_parameters(
-    dimensions_list: list[tuple[int, ...]], dtype: DTypeLike = np.int64
+    dimensions_list: list[tuple[int, ...]], dtype: Any = np.int64
 ) -> list[NDArray[Any]]:
     """Generate zero parameters based on the dimensions list."""
     return [np.zeros(dimensions, dtype=dtype) for dimensions in dimensions_list]
