c463fb154ea23ca8695c2cb8226736277f7c30da
RabeyaMuna <rabeyakhatun.muna@mail.concordia.ca>
2026-09-23 15:52:56 -0400

    xuyisen__diff__id_127

--- framework/py/flwr/common/secure_aggregation/ndarrays_arithmetic.py
+++ framework/py/flwr/common/secure_aggregation/ndarrays_arithmetic.py
@@ -18,48 +18,49 @@
 from typing import Any, Union
 
 import numpy as np
-from numpy.typing import DTypeLike, NDArray
+from numpy.typing import DTypeLike
+from flwr.common.typing import NDArray
 
 
-def factor_combine(factor: int, parameters: list[NDArray[Any]]) -> list[NDArray[Any]]:
+def factor_combine(factor: int, parameters: list[NDArray[Any, Any]]) -> list[NDArray[Any, Any]]:
     """Combine factor with parameters."""
     return [np.array([factor])] + parameters
 
 
 def factor_extract(
-    parameters: list[NDArray[Any]],
-) -> tuple[int, list[NDArray[Any]]]:
+    parameters: list[NDArray[Any, Any]],
+) -> tuple[int, list[NDArray[Any, Any]]]:
     """Extract factor from parameters."""
-    return parameters[0][0], parameters[1:]
+    return int(parameters[0][0]), parameters[1:]
 
 
-def get_parameters_shape(parameters: list[NDArray[Any]]) -> list[tuple[int, ...]]:
+def get_parameters_shape(parameters: list[NDArray[Any, Any]]) -> list[tuple[int, ...]]:
     """Get dimensions of each NDArray in parameters."""
     return [arr.shape for arr in parameters]
 
 
 def get_zero_parameters(
     dimensions_list: list[tuple[int, ...]], dtype: DTypeLike = np.int64
-) -> list[NDArray[Any]]:
+) -> list[NDArray[Any, Any]]:
     """Generate zero parameters based on the dimensions list."""
     return [np.zeros(dimensions, dtype=dtype) for dimensions in dimensions_list]
 
 
 def parameters_addition(
-    parameters1: list[NDArray[Any]], parameters2: list[NDArray[Any]]
-) -> list[NDArray[Any]]:
+    parameters1: list[NDArray[Any, Any]], parameters2: list[NDArray[Any, Any]]
+) -> list[NDArray[Any, Any]]:
     """Add two parameters."""
     return [parameters1[idx] + parameters2[idx] for idx in range(len(parameters1))]
 
 
 def parameters_subtraction(
-    parameters1: list[NDArray[Any]], parameters2: list[NDArray[Any]]
-) -> list[NDArray[Any]]:
+    parameters1: list[NDArray[Any, Any]], parameters2: list[NDArray[Any, Any]]
+) -> list[NDArray[Any, Any]]:
     """Subtract parameters from the other parameters."""
     return [parameters1[idx] - parameters2[idx] for idx in range(len(parameters1))]
 
 
-def parameters_mod(parameters: list[NDArray[Any]], divisor: int) -> list[NDArray[Any]]:
+def parameters_mod(parameters: list[NDArray[Any, Any]], divisor: int) -> list[NDArray[Any, Any]]:
     """Take mod of parameters with an integer divisor."""
     if bin(divisor).count("1") == 1:
         msk = divisor - 1
@@ -68,14 +69,14 @@ def parameters_mod(parameters: list[NDArray[Any]], divisor: int) -> list[NDArray
 
 
 def parameters_multiply(
-    parameters: list[NDArray[Any]], multiplier: Union[int, float]
-) -> list[NDArray[Any]]:
+    parameters: list[NDArray[Any, Any]], multiplier: Union[int, float]
+) -> list[NDArray[Any, Any]]:
     """Multiply parameters by an integer/float multiplier."""
     return [parameters[idx] * multiplier for idx in range(len(parameters))]
 
 
 def parameters_divide(
-    parameters: list[NDArray[Any]], divisor: Union[int, float]
-) -> list[NDArray[Any]]:
+    parameters: list[NDArray[Any, Any]], divisor: Union[int, float]
+) -> list[NDArray[Any, Any]]:
     """Divide weight by an integer/float divisor."""
     return [parameters[idx] / divisor for idx in range(len(parameters))]
